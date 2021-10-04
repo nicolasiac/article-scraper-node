ea7744a7b4a1834b63fa500d52dd04b96a7d118d
nicolasiac <nicolasiac@hotmail.com>
2021-10-04 12:27:50 +0300

    headless true

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ async function autoScroll(page) {
 
 router.get('/fetch', async (req, res, next) => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
